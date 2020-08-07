7788a51ed1d0040a23ad527d8a5c17aaf3a54eda
Joao Bordignon <joaobordignon@gmail.com>
2020-08-06 21:06:33 -0400

    Objetos aula 1 - Classe, Objeto e Metodo init

---
+++ Objetos/Aula1/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos.Aula1
+{
+    public class Aluno
+    {
+        public string nome;
+        public int idade;
+        public string email;
+        public string endereco;
+        public string matricula;
+
+        public void mostraOsDadosDeAlunoNaTela()
+        {
+            Console.WriteLine("Nome: "+ nome );
+            Console.WriteLine("Idade: " + idade);
+            Console.WriteLine("email: " + email);
+            Console.WriteLine("endereco: " + endereco);
+            Console.WriteLine("Matricula: " + matricula);
+        }
+    }
+}
---
+++ Objetos/Aula1/programa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos.Aula1
+{
+    public class programa
+    {
+        static void Main (string[] args)
+        {
+            Aluno aluno1 = new Aluno();
+            aluno1.nome = "Joao";
+            aluno1.idade = 32;
+            aluno1.email = "joaobordignon@gmail.com";
+            aluno1.endereco = "512-500 rue Axxxd";
+            aluno1.matricula = "";
+
+            Professor professor1 = new Professor();
+            professor1.nome = "Luana";
+            professor1.idade = 31;
+            professor1.endereco = "rua do tal, 45";
+            professor1.email = "luana@gmail.com";
+            professor1.disciplina = "C#";
+
+            aluno1.mostraOsDadosDeAlunoNaTela();
+            professor1.mostraOsDadosDeProfessorNaTela();
+
+
+            Console.ReadKey();
+        }
+    }
+}
