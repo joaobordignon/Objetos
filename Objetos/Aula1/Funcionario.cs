5df58812fe3a2553073af505b65dd353574ba8b0
Joao Bordignon <joaobordignon@gmail.com>
2020-08-11 19:02:39 -0400

    Add Exercicio aula01 Classe Funcionario.cs

---
+++ Objetos/Aula1/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos.Aula1
+{
+    public class Funcionario
+    {
+        public string nome;
+        public string sobrenome;
+        public string email;
+        public int idade;
+
+        public void MostraEmail()
+        {
+            Console.WriteLine($"E-mail: {email}");
+        }
+
+        public void MostraODobroDaIdade()
+        {
+            Console.WriteLine($"O Dobro da idade é {idade*2}.");
+        }
+
+        public void MostraNomeESobrenome()
+        {
+            Console.WriteLine($"{nome} {sobrenome}.");
+        }
+    }
+
+}
